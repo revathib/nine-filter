8c13ceb8d0051d968ce8d7364a9017e554b1ef71
revathipathuri <revathi.bandi@mtuity.com>
2020-02-11 12:01:42 +1100

    simple express app

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-var express = require('express');
-var app = express();
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+let express = require('express');
+let app = express();
+let filter = require('./src/filter');
+const cors = require('cors');
+
+const corsOptions = {
+  allowedHeaders: ['Origin', 'Content-Type', 'Accept'],
+  methods: ['POST']
+};
+
+app.use(cors(corsOptions));
+app.post('/', async (request, response) => {
+  console.log(request);
+  return response.send('success');
+});
+const port = process.env.PORT || '4201';
+app.set('port', port);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
